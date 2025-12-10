4cc604ef5774ada4a25e09e0b0593b0526293c2f
rohithreddy479 <157205943+rohithreddy479@users.noreply.github.com>
2025-12-10 08:41:14 +0530

    Update DATABASE_URL for database connection

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Hitam%40123@localhost:5432/plantpal"
+DATABASE_URL = "postgresql://postgres: password localhost:5432/db name"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
